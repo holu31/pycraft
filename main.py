42bb392a5494a17ad65840920c677b8177a7a670
Daniil Lebedev <93409280+holu31@users.noreply.github.com>
2022-07-22 15:42:47 +0300

    shader settings

--- main.py
+++ main.py
@@ -8,18 +8,20 @@ from settings import *
 blocks = []
 
 app = Ursina()
-sun = SunLight(direction = (-0.7, -0.9, 0.5), resolution = 2048)
-ambient = AmbientLight(color = Vec4(0.5, 0.55, 0.66, 0) * 0.75)
-render.setShaderAuto()
-sky = Sky(texture='sky0')
+if SHADERS:
+    sun = SunLight(direction = (-0.7, -0.9, 0.5), resolution = 2048) # shadow
+    ambient = AmbientLight(color = Vec4(0.5, 0.55, 0.66, 0) * 0.75) # light
+    render.setShaderAuto()
+sky = Sky(texture='textures/sky0')
 window.fullscreen = FULLSCREEN
 window.size = (WIDTH,HEIGHT)
-window.fps_counter.enabled = False
+window.fps_counter.enabled = True
 window.title = 'RuMine'
 window.exit_button.visible = False
+chanks = 2
 
-for x in range(16):
-    for z in range(16):
+for x in range(16 * 2):
+    for z in range(16 * 2):
         blocks.append(Block((x, 0, z)))
 
 player = Player()
